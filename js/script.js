f283653ec1d818d817b5a8af930c10544af23766
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-01-08 17:21:38 -0300

    incompleto contacto, no sale imagen

--- js/script.js
+++ js/script.js
@@ -16,9 +16,18 @@ function tipear(cadena, ms) {
 
 
 function carga_estudio(estudios){
+    //recorre la lista estudio y los imprime en el html
     let lista_cursos = document.getElementById("lista_curso");
     for (estudio of estudios){
         lista_cursos.innerHTML += `<li>${estudio.nombreCurso} - ${estudio.entidad} - ${estudio.anio}</li>`;
+    }
+}
+
+function cargar_redes(redes, elemento = document.getElementById("lista_redes")){
+    //recorre la lista de redes y los imprime en el html
+    //puede recibir un elemento al cual se insertará o puede tener por defecto la lista de redes
+    for (red of redes){
+        elemento.innerHTML += `<li><a href=${red.link}> <img src=${red.imagen}/> </a></li>`;
     }
 }
 
@@ -29,10 +38,18 @@ window.addEventListener('load', function () {
         {"nombreCurso" : "Introducción a Java", "entidad" : "EducaciónIT", "anio": "2023"},
         {"nombreCurso" : "Desarrollo Web con HTML", "entidad" : "EducaciónIT", "anio": "2023"},
         {"nombreCurso" : "Introducción al paradigma de objetos", "entidad" : "EducaciónIT", "anio": "2023"},]
+
+    let redes = [
+        {"nombreRed" : "GMAIL", "link" : false, "imagen" : "img/iconos/gmail.png"},
+        {"nombreRed" : "LinkedIn", "link" : "https://www.linkedin.com/in/matias-h-diaz-98a89123b/", "imagen" : "img/iconos/linkedin.png"},
+        {"nombreRed" : "Github", "link" : "https://github.com/MatiasHernanDiaz", "imagen" : "img/iconos/github.png"},
+        {"nombreRed" : "Discord", "link" : false, "imagen" : "img/iconos/discord.png"}]
     
     tipear("¡Hola! Soy MATIAS H. DIAZ", 200);
     
-    carga_estudio(estudios);
+    carga_estudio(estudios, document.getElementById("lista_redes"));
+
+    cargar_redes(redes);
 
 });
 
