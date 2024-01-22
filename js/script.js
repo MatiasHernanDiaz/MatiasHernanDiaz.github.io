78ca1a57a7ad55b7e0a009c44acf7bc1aef326da
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-01-21 22:35:22 -0300

    FALTA CSS DE HERRAMIENTAS

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ function escribirCadena(cadena, elem, tiempo, terminacion = false) {
     for (let i = 0; i < cadena.length; i++) {
         setTimeout(() => {
             elem.innerHTML = cadena.substring(0, i + 1) + '|';
-            
-            if(i === (cadena.length - 1) && terminacion){
-                elem.innerHTML = cadena;    
+
+            if (i === (cadena.length - 1) && terminacion) {
+                elem.innerHTML = cadena;
             }
 
         }, tiempo * i);
     }
-    
+
 }
 
 
@@ -61,7 +61,7 @@ function maquina(cadena, elem, tiempo) {
 }
 function variasMaquinas(cadenas, elem, tiempo) {
     //acepta un array de dos cadenas las cual alterna para escribirlas y borrarlas
-    
+
     const periodo = ((cadenas[0].length + 1) * tiempo * 2) + 1000;
     const periodoNuevo = ((cadenas[1].length + 1) * tiempo * 2) + 1000;
 
@@ -82,6 +82,9 @@ function variasMaquinas(cadenas, elem, tiempo) {
     }, periodoNuevo)
 }
 
+
+/*SECCION ESTUDIO*/
+
 function carga_estudio(estudios) {
     //recorre la lista estudio y los imprime en el html
     let lista_cursos = document.getElementById("lista_curso");
@@ -89,6 +92,9 @@ function carga_estudio(estudios) {
         lista_cursos.innerHTML += `<li>${estudio.nombreCurso} - ${estudio.entidad} - ${estudio.anio}</li>`;
     }
 }
+
+
+/*SECCION CONTACTO */
 
 function cargar_redes(redes, elemento = document.getElementById("lista_redes")) {
     //recorre la lista de redes y los imprime en el html
@@ -102,7 +108,7 @@ function cargar_redes(redes, elemento = document.getElementById("lista_redes")) 
     }
 }
 
-
+/*NAVEGADOR */
 
 function toggleClass(elem, className) {
     if (elem.className.indexOf(className) !== -1) {
@@ -154,50 +160,99 @@ function toggleMenuDisplay(e) {
     toggleClass(icon, 'rotate-90');
 }
 
-/*
-function cargar_herramientas(herramientas){
-    pizarra = document.getElementById('lista_resultado');
-    
-    alert( herramientas[0].lenguaje);
-    for(const herramienta of herramientas){
-        pizarra.innerHTML += `<li class='items_resultado'> ${herramienta.lenguaje}</li>`
-    }
-}
-*/
 
 
-function buscar(event) {
-    let pizarra = document.getElementById('lista_resultado');
-    let input = event.currentTarget;
-    let cantidad = document.getElementById('checking_resultado_herramienta');
-    valor = input.value.toLowerCase();
-    let contador = 0;
 
-    const herramientas = [
-        { 'lenguaje': 'python', 'proyecto': [1, 2] },
-        { 'lenguaje': 'javascript', 'proyecto': [3, 4] },
-        { 'lenguaje': 'HTML', 'proyecto': [3, 4] },
-        { 'lenguaje': 'CSS', 'proyecto': [3, 4] },
-        { 'lenguaje': 'java', 'proyecto': [3, 4] },
-        { 'lenguaje': 'C', 'proyecto': [3, 4] },
-        { 'lenguaje': 'C++', 'proyecto': [3, 4] }]
+/*SECCION HERRAMIENTAS */
 
+function cargarHerramientas(lista, elemento) {
+    for (const herramienta of lista) {
+        elemento.innerHTML += `<li class='item_lista'>
+                                    <div class="tecnologia"> <img class="logo" src=${herramienta.logo}> </div>
+                                    <div class="tecnologia"> ${herramienta.lenguaje} </div> 
+                                    <div> ${herramienta.tecnologia}</div>
+                                    <div> <p>${herramienta.resumen}</p> </div>
+                                    <div> <a hrf=${herramienta.proyecto}> link </a> </div>
+                                    <div>  </div>
+                                </li>`
+    };
+};
 
-    for (const herramienta of herramientas) {
-        if (herramienta.lenguaje.toLowerCase().includes(valor) && (valor !== '')) {
-            contador++;
-            pizarra.innerHTML += `<li class='lista_herramienta'>${herramienta.lenguaje}</li>`
-
+function subFiltro(listaHerramientas, valor, total, orden) {
+    //recorre la lista de herramientas
+    //cada herramienta crea una lista de 3 chidren.
+    //el 0 es lenguaje, 1 tecnologia, 2 proyecto
+    //dependiendo el orden elegido machea y muestra. si no oculta los los negativos
+    for (const herramienta of listaHerramientas) {
+        const hijos = herramienta.children;
+        
+        if (hijos[orden].textContent.toLowerCase().includes(valor) && (valor !== '')) {
+            herramienta.style.display = 'list-item';
         }
         else {
-            //pizarra.innerHTML = ``;
+            herramienta.style.color = 'black';
+            herramienta.style.display = 'none';
         }
-
-
     }
-    cantidad.innerHTML = `${contador}`;
-
+    for (let i of listaHerramientas) {
+        if (i.style.display !== 'none') {
+            total++;
+        }
+    }
+    return total;
 }
+
+function filtro(event, listaHerramientas) {
+    //tomando el valor del input recorre la lista de herramientas y se fija con cual coincide
+    //y la imprime en una lista.
+    const elemento = event.currentTarget;
+    const radios = document.querySelectorAll('input[name="tipo_busqueda"]');
+    let valor = elemento.value;
+    let total = 0;
+    valor = valor.toLowerCase();
+
+    if (radios.length > 0) {
+        //selecciono una opcion
+        if (radios[0].checked) {
+            total = subFiltro(listaHerramientas, valor, total, 1);
+        }
+        else if (radios[1].checked) {
+            total = subFiltro(listaHerramientas, valor, total, 2);
+        }
+        else {
+            total = subFiltro(listaHerramientas, valor, total, 3);
+        }
+    }
+
+    if (valor === '') {
+        for (const herramienta of listaHerramientas) {
+            herramienta.style.color = 'black';
+            herramienta.style.display = 'none';
+            if (total > 0) {
+                total--;
+            }
+        }
+    }
+    console.log(total);
+};
+
+function todos(event) {
+    /* ESTA FUNCION ESTA REPETIDO, REFACTORIZAR */
+    const boton = event.currentTarget;
+    const herramientas = document.getElementsByClassName('item_lista');
+    if (boton.checked) {
+        for (const herramienta of herramientas) {
+            const hijos = herramienta.children;
+            herramienta.style.display = 'list-item';
+        }
+    }
+    else {
+        herramienta.style.color = 'black';
+        herramienta.style.display = 'none';
+    }
+}
+
+
 
 
 window.addEventListener('load', function () {
@@ -215,13 +270,10 @@ window.addEventListener('load', function () {
         { 'nombreRed': 'Discord', 'link': false, 'imagen': 'img/iconos/discord.png', 'noLink': 'matiashdiaz' }]
 
     const herramientas = [
-        { 'lenguaje': 'python', 'proyecto': [1, 2] },
-        { 'lenguaje': 'javascript', 'proyecto': [3, 4] },
-        { 'lenguaje': 'HTML', 'proyecto': [3, 4] },
-        { 'lenguaje': 'CSS', 'proyecto': [3, 4] },
-        { 'lenguaje': 'java', 'proyecto': [3, 4] },
-        { 'lenguaje': 'C', 'proyecto': [3, 4] },
-        { 'lenguaje': 'C++', 'proyecto': [3, 4] }]
+        { 'lenguaje': 'Python', 'tecnologia': 'Vanilla', 'proyecto': ['https://github.com/MatiasHernanDiaz/Analisis_de_datos_Walmark-csv'], 'resumen':['Analisis de datos de Walmark'], 'imagenes': [['/img/proyectos/img_proyecto_python_analisis.png']], 'logo' : '/img/iconos/python.png' },
+        { 'lenguaje': 'Javascript', 'tecnologia': 'Vanilla', 'proyecto': ['5', '6'], 'resumen' : ['HTML, CSS, JS, eventos'], 'imagenes':[['falta_imagen']], 'logo' : '/img/iconos/js_html_css.png'},
+        { 'lenguaje': 'C', 'tecnologia': 'Vanilla', 'proyecto': ['https://github.com/MatiasHernanDiaz/CRUD-ANSI-C'], 'resumen' : [['CRUD de corredores']], 'imagenes' : [['falta_imagen']], 'logo' : '/img/iconos/logo_c.png' },
+        { 'lenguaje': 'Assembly', 'tecnologia': 'Vanilla', 'proyecto': ['https://github.com/MatiasHernanDiaz/game-x'], 'resumen' : [['Juego']], 'imagenes' : [['falta_imagen']], 'logo' : '/img/iconos/logo_ASM.png'}];
 
 
     const elem = document.getElementById('hola');
@@ -231,28 +283,47 @@ window.addEventListener('load', function () {
     const tiempoTipeo = 150;
     const dropdownTitle = document.querySelector('.dropdown .title');
     const dropdownOptions = document.querySelectorAll('.dropdown .option');
-    
+
     //NAVEGADOR
     dropdownTitle.addEventListener('click', toggleMenuDisplay);
-    
+
     //SECCION PRESENTACION
     escribirCadena(presentacion, elem, tiempoTipeo, terminacion = true);
-    this.setTimeout(()=>{
+    this.setTimeout(() => {
         variasMaquinas(profesion, elemProfesiones, tiempoTipeo);
-    },presentacion.length * tiempoTipeo); //es el tiempo que tarda en escribir la presentacion
+    }, presentacion.length * tiempoTipeo); //es el tiempo que tarda en escribir la presentacion
 
-    
+
     //SEC HERRAMIENTA
     //cargar_herramientas(herramientas);    
-    const buscador = document.getElementById('id_buscador_herramientas');
-    buscador.addEventListener('keyup', buscar);
-    
+    //const buscador = document.getElementById('id_buscador_herramientas');
+    //buscador.addEventListener('keyup', buscar);
+
+
+    const elemento = document.getElementById('lista_herramientas');
+    const input = document.getElementById('buscador');
+    const listaHerramientas = document.getElementsByClassName('item_lista');
+    const radioTodos = document.getElementById('todos');
+
+
+    cargarHerramientas(herramientas, elemento);
+
+
+    input.addEventListener('keyup', function (event) {
+        filtro(event, listaHerramientas);
+    });
+
+    radioTodos.addEventListener('click', todos);
+
+
+
     //SEC ESTUDIOS
     carga_estudio(estudios, document.getElementById("lista_redes"));
-    
-    
+
+
     //CONTACTO
     cargar_redes(redes);
+
 
 
 });
